26855f2f098e987e9c2589d8bb81b188c517d515
Realitaa <rezamuliaputra927@gmail.com>
2024-12-17 09:55:31 +0700

    initial setup

---
+++ app/Views/karyawan/home.php
@@ -0,0 +1,8 @@
+<?= $this->extend('template/template') ?>
+
+<?= $this->section('content') ?>
+
+    <!-- Naufal mengerjakan folder ini -->
+    <p class="h1">Hello World!</p>
+
+<?= $this->endSection('content') ?>
