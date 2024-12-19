22b04cc45fa15d966ad27e04feac139e9d47d95a
Realitaa <rezamuliaputra927@gmail.com>
2024-12-19 08:07:05 +0700

    Menambahkan addKaryawan.php

---
+++ app/Views/admin/addKaryawan.php
@@ -0,0 +1,16 @@
+<?= $this->extend('template/templateAdmin'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="bg-light">
+
+<!-- Buat form tambah karyawan disini. Form yang diminta adalah sebagai berikut:
+    id
+    nama pengguna
+    Email
+    no Telepon
+    Jabatan -->
+
+</div>
+
+<?= $this->endSection('content'); ?>
